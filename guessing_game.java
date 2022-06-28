02ece06f41087568a0fe5e6db2364de4fb48a58c
SantoshMThakuri <iamsantoshmthakuri@gmail.com>
2022-06-27 23:36:19 -0400

    complete

--- guessing_game.java
+++ guessing_game.java
@@ -1,15 +1,22 @@
 import java.util.*;
+
 public class guessing_game{
+
     public static void main(String[]args){
-        System.out.println("Enter the alphabts");
-        String user =  sc.next().charAT(0);
-        if(user=="a" || user=="b" || user== "c"|| user=="o"){
-            System.out.println("Character is vowel ="+ user);
+
+        System.out.println("Enter the alphabets");
+        // to get the scanner object we need to import scanner and create a new method.
+
+        Scanner scan = new Scanner(System.in);
+        char user = scan.next().charAt(0); // this is to take input
+
+        if(user=='a' || user=='e' || user=='i'|| user=='o' || user=='u' || user=='A'|| user=='E'|| user=='I'||user=='O' || user=='U')
+        {
+            System.out.println("The Character" +" " + user+ " "+"is vowel.");
         }
-        else{
-            System.out.println("Character is consonent =" +user);
+        else {
+            System.out.println("The Character" +" " +user+ " "+"is consonent");
         }
     }
 
-
-}
+    }
