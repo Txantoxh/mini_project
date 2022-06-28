84fa425030f7c61ba331a0258452da3aebde0d0b
SantoshMThakuri <iamsantoshmthakuri@gmail.com>
2022-06-27 23:15:03 -0400

    game

--- guessing_game.java
+++ guessing_game.java
@@ -1,7 +1,13 @@
 public class guessing_game{
-    public static void guessing_game(Strings[]args){
+    public static void main(String[]args){
         System.out.println("Enter the alphabts");
-
+        String user =  sc.next().charAT(0);
+        if(user=="a" || user=="b" || user== "c"|| user=="o"){
+            System.out.println("Character is vowel ="+ user);
+        }
+        else{
+            System.out.println("Character is consonent =" +user);
+        }
     }
 
 
